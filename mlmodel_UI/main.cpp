41b73c502fb5c690cb019214f272115ac3c65ff0
Bart van Stratum <bart@vanstratum.com>
2010-03-05 11:54:36 +0100

    Interface + start CPP code

---
+++ mlmodel_UI/main.cpp
@@ -0,0 +1,10 @@
+#include <QtGui/QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow main;
+    main.show();
+    return a.exec();
+}
---
+++ mlmodel_UI/mlmodel_main.pro
@@ -0,0 +1,8 @@
+TARGET = mlmodel_main
+TEMPLATE = app
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+FORMS    += mainwindow.ui
