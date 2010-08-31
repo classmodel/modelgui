b9a3daad8ae958e0bfd68a8308901cba740346cd
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2010-08-31 17:36:10 +0200

    Removed limitation of RH

--- model.cpp
+++ model.cpp
@@ -461,10 +461,7 @@ void model::runmlmodel()
   double Ttop    = theta / pow(Ps / Ptop,Rd / cp);
   double esattop = 0.611e3 * exp((Lv / Rv) * ((1. / 273.15)-(1. / Ttop)));
   double etop    = q * Ptop / 0.622;
-  if ((etop / esattop) > 1.)
-    RHtop        = 1.;
-  else
-    RHtop        = etop / esattop;
+  RHtop          = etop / esattop;
 }
 
 void model::intmlmodel()
