da64d5eba9090429c25c1576dd24a8ebf7a6b42b
Bart van Stratum <bart@vanstratum.com>
2010-09-01 13:44:42 +0200

    deleted duplicate line

--- plotwindow.cpp
+++ plotwindow.cpp
@@ -239,7 +239,6 @@ plotwindow::plotwindow(QMap<int, modelrun> *runs, QList<int> *initialselected, Q
       treeitem->setText(3, description);
       treeitem->setText(4, id);
       treegroup->addChild(treeitem);
-      treegroup->addChild(treeitem);
     }
   }
 
