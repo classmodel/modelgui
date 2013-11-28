6ea1d340d5b21a80e6a8cffad726e05aec884a67
Bart van Stratum <bart@vanstratum.com>
2013-11-28 12:30:47 +0100

    Added licence information to all .cpp and .h files

--- plotwindow.cpp
+++ plotwindow.cpp
@@ -1,3 +1,27 @@
+/*
+ * CLASS
+ * Copyright (c) 2010-2013 Meteorology and Air Quality section, Wageningen University and Research centre
+ * Copyright (c) 2011-2013 Jordi Vila-Guerau de Arellano
+ * Copyright (c) 2011-2013 Chiel van Heerwaarden
+ * Copyright (c) 2011-2013 Bart van Stratum
+ * Copyright (c) 2011-2013 Kees van den Dries
+ *
+ * This file is part of CLASS
+ *
+ * CLASS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * CLASS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with CLASS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "plotwindow.h"
 #include "subplot.h"
 #include "ui_subplot.h"
